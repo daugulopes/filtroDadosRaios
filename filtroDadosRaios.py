5be365a4a51e523f587af50b4b0eb7db7e088275
daugulopes <80639864+daugulopes@users.noreply.github.com>
2021-03-14 14:00:00 -0300

    Update filtroDadosRaios.py

--- filtroDadosRaios.py
+++ filtroDadosRaios.py
@@ -1,31 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[248]:
-
-
-#teste  importar pandas e dados
-import pandas as pd
-import glob
-df = pd.read_csv("starnet_2014-12-31_flash.dat", sep="\s+",header=None, prefix="Coluna_" )
-#df = pd.concat(map(pd.read_csv, glob.glob('starnet*.dat')),ignore_index=True, axis=1) 
-
-
-# In[168]:
-
-
-df.head()
-
-
-# In[169]:
-
-
-#filtrando pela coluna 11 de qualidade
-filtro1 = df[df.Coluna_11==0][["Coluna_0","Coluna_1","Coluna_2","Coluna_3","Coluna_4","Coluna_5","Coluna_7","Coluna_8","Coluna_11"]].head()
-
-
-# In[250]:
-
 
 #código filtro dados
 
